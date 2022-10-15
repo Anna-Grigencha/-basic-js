50eacaea78a078964017e37737bde51b2a08a393
Anna-Grigencha <grigencha10@gmail.com>
2022-10-15 22:42:36 +0300

    feat: add mac-address

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
+ function deleteDigit(n) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  n = String(n).split("");
+  n.sort((a,b) => b-a);
+  n.pop();
+  n = Number(n.join(""));
+  console.log(typeof n);
+  return n;
 }
 
 module.exports = {
