68e5e08a0f564f499405bf1048d98df52d483f59
Anna-Grigencha <grigencha10@gmail.com>
2022-10-15 17:00:34 +0300

    feat: mpdified transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,6 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
  function transform(arr) {
+  if (Array.isArray(arr) === false) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+    else {
+  
   let resultWithEmpty = [];
   let result = [];
   //throw new NotImplementedError('Not implemented');
@@ -23,7 +28,7 @@ for (let i = 0; i< arr.length; i++){
     resultWithEmpty.push(arr[i+1]);
   }
 
-  if (arr[i] === '--double-prev' && arr[i-1] !== undefined) {
+  if (arr[i] === '--double-prev' && arr[i-1] !== undefined && arr[i-2] !== '--discard-next') {
     resultWithEmpty.push(arr[i-1]);
   }
   if (arr[i]=== '--discard-prev' && resultWithEmpty.length !==0){
@@ -35,14 +40,11 @@ for (let i = 0; i< arr.length; i++){
    i = i+1;
 
   }
-  
   else if (arr[i] !=='--double-next' && arr[i] !=='--double-prev' && arr[i] !=='--discard-prev' && arr[i] !== '--discard-next'){
     resultWithEmpty.push(arr[i]);
   }
 }
-console.log(resultWithEmpty);
 
-//return resultWithEmpty;
 for (let i = 0; i <resultWithEmpty.length; i++ ) {
 if (resultWithEmpty[i] !== "empty"){
   result.push(resultWithEmpty[i]);
@@ -50,8 +52,8 @@ if (resultWithEmpty[i] !== "empty"){
 }
 console.log(result);
 return result;
+  }
  }
-
 module.exports = {
   transform
 };
