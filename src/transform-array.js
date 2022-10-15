5e3696a17824936abbdef3322b1ec984efb143e3
Anna-Grigencha <grigencha10@gmail.com>
2022-10-15 16:22:42 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+ function transform(arr) {
+  let resultWithEmpty = [];
+  let result = [];
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+for (let i = 0; i< arr.length; i++){
+  if (arr[i] === '--double-next' && arr[i+1] !== undefined) {
+    resultWithEmpty.push(arr[i+1]);
+  }
+
+  if (arr[i] === '--double-prev' && arr[i-1] !== undefined) {
+    resultWithEmpty.push(arr[i-1]);
+  }
+  if (arr[i]=== '--discard-prev' && resultWithEmpty.length !==0){
+    resultWithEmpty.pop();
+  }
+  if (arr[i] === '--discard-next' && arr.length !== i+1){
+    
+    resultWithEmpty.push("empty");
+   i = i+1;
+
+  }
+  
+  else if (arr[i] !=='--double-next' && arr[i] !=='--double-prev' && arr[i] !=='--discard-prev' && arr[i] !== '--discard-next'){
+    resultWithEmpty.push(arr[i]);
+  }
 }
+console.log(resultWithEmpty);
+
+//return resultWithEmpty;
+for (let i = 0; i <resultWithEmpty.length; i++ ) {
+if (resultWithEmpty[i] !== "empty"){
+  result.push(resultWithEmpty[i]);
+}
+}
+console.log(result);
+return result;
+ }
 
 module.exports = {
   transform
